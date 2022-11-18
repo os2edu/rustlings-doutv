b48247e8409bdb061f5cdb5401cd9ef6bd112bbb
backdoor705.eth <20609724+doutv@users.noreply.github.com>
2022-11-18 01:50:26 +0000

    exercise

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number: u32) -> u32 {
+    match number {
+        _number if number <= 40 => _number * 2,
+        _ => number
+    }
+}
 
 // Don't modify this function!
 #[test]
